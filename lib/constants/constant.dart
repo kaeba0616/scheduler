187fc9129e2a42dba87c0a9e2d2a7ed4f59bf2b7
ms <alstjq1012@gmail.com>
2023-12-06 15:38:57 +0900

    intial commit
    처음 커밋

---
+++ lib/constants/constant.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static String PF_API = "36a86c6d-312d-42d9-9b8c-a9159e5a95ae";
+  static String oneDayClass_API = "648d45d4-01b9-4111-83b2-9fc647ac0bff";
+  static String actPerformance_API = '74b23ee9-a136-4960-b48a-8fb74e8fdee1';
+}
